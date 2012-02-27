cef590c49093b1c88d203f891902a0d34d91b311
Maxence Dunnewind <maxence@dunnewind.net>
2012-02-27 22:24:23 +0100

    compute real width even when ansi codes are used

--- lib/terminal-table/core_ext.rb
+++ lib/terminal-table/core_ext.rb
@@ -1,7 +1,14 @@
 
 class String
   def align position, length
-    self.__send__ position, length
+    delta = 0
+    if self.to_s != nil and length < self.length
+        str = self.to_s.gsub(/\x1b(\[|\(|\))[;?0-9]*[0-9A-Za-z]/, '')
+        str = str.gsub(/\x1b(\[|\(|\))[;?0-9]*[0-9A-Za-z]/, '')
+        str = str.gsub(/[\x03|\x1a]/, '')
+        delta = self.length - str.length
+    end
+    self.__send__ position, length + delta
   end
   alias_method :left, :ljust
   alias_method :right, :rjust
